e6c1fd9fe3bb69bffe9b4dc331b6a12208a7753e
wangkaill <51182173+wangkaill@users.noreply.github.com>
2020-09-27 18:22:13 +0800

    在线更新

--- ch02-javase-maven/src/main/java/com/bjpowernode/HellloMaven.java
+++ ch02-javase-maven/src/main/java/com/bjpowernode/HellloMaven.java
@@ -14,6 +14,7 @@ public class HellloMaven {
         System.out.println("重新创建分支");
         System.out.println("分支再添加");
         System.out.println("主干添加");
+        System.out.println("在线更新");
     }
     public int add(int a, int b) {
         return  a + b;
